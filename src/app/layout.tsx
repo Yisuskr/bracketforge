4a6bd633d91e224f7d84091cb38dab95b817af4f
Yisuskr <ejesus1122@gmail.com>
2026-08-29 18:04:58 +0100

    Fix root layout hydration mismatch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
       lang="es"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" style={{ isolation: "isolate" }}>
+        {children}
+      </body>
     </html>
   );
 }
